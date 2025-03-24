0cc7475062739dae2976a8ec86d6682faa4c0626
Avramis04 <Avramiz.kontou@icloud.com>
2025-03-24 21:05:31 +0200

    commi and push

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,11 @@
+/** \file Source.cpp
+* \brief drawing shapes program.
+* \details this program will draw shapes based on the user input.
+* \author AVRAMIS KONTOU
+* \version 0.1
+* \date 24 /3/2025
+* \copyright UNIVERSITY OF NICOSIA.
+*/
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -12,7 +20,10 @@ void drawSquareFilled(int size, char ch);
 void drawRectangle(int height, int length, char ch);
 void drawRectangleFilled(int height, int length, char ch);
 void drawShapes(int numShapes);
-
+/**
+* Main function providing a menu for user to draw different shapes.
+* @return 0 upon successful execution.
+*/
 int main() {
     srand(time(NULL)); // for generated random number 
     int option;
@@ -64,18 +75,30 @@ int main() {
     } while (option != 6);
     return 0;
 }
-
+/**
+* Draws a horizontal line.
+* @param length The number of characters to print.
+* @param ch The character to use for drawing.
+*/
 void drawHorizontalLine(int length, char ch) {
     for (int i = 0; i < length; i++)
         cout << ch;
     cout << endl;
 }
-
+/**
+* Draws a vertical line.
+* @param height The number of lines to print.
+* @param ch The character to use for drawing.
+*/
 void drawVerticalLine(int height, char ch) {
     for (int i = 0; i < height; i++)
         cout << ch << endl;
 }
-
+/**
+* Draws an unfilled square.
+* @param size The width and height of the square.
+* @param ch The character to use for drawing.
+*/
 void drawSquare(int size, char ch) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++)
@@ -83,11 +106,20 @@ void drawSquare(int size, char ch) {
         cout << endl;
     }
 }
-
+/**
+* Draws a filled square.
+* @param size The width and height of the square.
+* @param ch The character to use for drawing.
+*/
 void drawSquareFilled(int size, char ch) {
     drawSquare(size, ch);
 }
-
+/**
+* Draws an unfilled rectangle.
+* @param height The number of rows.
+* @param length The number of columns.
+* @param ch The character to use for drawing.
+*/
 void drawRectangle(int height, int length, char ch) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < length; j++)
@@ -95,11 +127,19 @@ void drawRectangle(int height, int length, char ch) {
         cout << endl;
     }
 }
-
+/**
+* Draws a filled rectangle.
+* @param height The number of rows.
+* @param length The number of columns.
+* @param ch The character to use for drawing.
+*/
 void drawRectangleFilled(int height, int length, char ch) {
     drawRectangle(height, length, ch);
 }
-
+/**
+* Generates and draws a given number of random shapes.
+* @param numShapes The number of shapes to generate.
+*/
 void drawShapes(int numShapes) {
     const int MAX_SHAPES = 10;
     numShapes = (numShapes > MAX_SHAPES) ? MAX_SHAPES : numShapes;
